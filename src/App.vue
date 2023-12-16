02d669cc6e7b6955485a6527ea0f4922cf7a5da6
AlessandroBilli <skillmanbungo@gmail.com>
2023-12-16 18:09:31 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script>
+import Header from './components/Header.vue'
+import Main from './components/Main.vue'
+import Cards from './components/Cards.vue'
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    Header,
+    Main,
+    Cards,
+
+  }
+}
+</script>
+
+<template></template>
+
+<style scoped></style>
